6693e2da3baf86165926063905e3bde41c3ae64a
Eduardo souza <106537002+EduardoSza@users.noreply.github.com>
2026-05-27 20:01:18 -0400

    Update celciusToFahrenheit.js

--- celciusToFahrenheit.js
+++ celciusToFahrenheit.js
@@ -2,7 +2,7 @@ function converterCelsiusParaFahrenheit(celsius) {
   if (typeof celsius !== "number") {
     throw new Error("A entrada deve ser um número.");
   }
-  return (celsius * 9) / 5 + 100;
+  return (celsius * 9) / 5 + 32;
 }
 
 module.exports = converterCelsiusParaFahrenheit;
