a441b2040caf335ccda5a4a8f0c04b23726ce66b
EduardoSza <eduardosouz466@gmail.com>
2026-06-29 16:19:39 -0400

    [Cenario 3] Alteracao intencional para causar falha nos testes

--- fahrenheitToCelcius.js
+++ fahrenheitToCelcius.js
@@ -3,8 +3,7 @@ function converterFahrenheitParaCelsius(fahrenheit) {
   if (typeof fahrenheit !== "number") {
     throw new Error("A entrada deve ser um número.");
   }
-  return ((fahrenheit - 32) * 5) / 9;
+  return ((fahrenheit - 32) * 5) / 9 + 10;
 }
 
 module.exports = converterFahrenheitParaCelsius;
-functionQuebrada(
